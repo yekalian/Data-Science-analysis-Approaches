2409897b8298e879550d516f4b08c9e23ec91289
z1qsx <3efv0om2wdc@163.com>
2015-05-15 22:43:15 +0800

    qt1

--- close.py
+++ close.py
@@ -4,10 +4,21 @@
 from PyQt5 import QtWidgets, QtGui#载入基本窗口模块,ICON
 import sys
 
+class MainWindow(QtWidgets.QMainWindow):
+		def __init__(self):
+			super(MainWindow, self).__init__()
+
+			self.status()
+
+		def status(self):
+			self.statusBar().showMessage("就绪")
+
+
 class view (QtWidgets.QWidget): # notice :
 	def __init__(self):
-		super().__init__()
-        
+		super(view,self).__init__()
+		self.MainWindow=MainWindow()
+		self.MainWindow.status
 		self.initUI()
 
 	def initUI(self):
@@ -18,11 +29,29 @@ class view (QtWidgets.QWidget): # notice :
 		QtWidgets.QToolTip.setFont(QtGui.QFont("Times", 10))#set tip
 		quit_button = QtWidgets.QPushButton("close",self)#set button
 		quit_button.setGeometry(10,10,60,35)#set button postion
-		quit_button.clicked.connect(QtWidgets.qApp.quit)#button clicked  connect 
+		quit_button.clicked.connect(self.closeEvent)#button clicked  connect 
+		screen =QtWidgets.QDesktopWidget().screenGeometry()#screen center
+		size =self.geometry()
+		self.move((screen.width()- size.width())/2, (screen.height() - size.height())/2)
+	def closeEvent(self,event):
+		reply = QtWidgets.QMessageBox.question(self,'ensure quit','Are you sure quit?',
+		                                       QtWidgets.QMessageBox.Yes,
+		                                       QtWidgets.QMessageBox.No)
+
+		if reply == QtWidgets.QMessageBox.Yes:
+			event.accpet()
+
+		else:
+			event.ignore()
+	
+
+
+
 
 if __name__=='__main__':  #pay attention to __
 
 	app = QtWidgets.QApplication(sys.argv)#application object
+	
 	view = view()
 	view.show()#show window of content
 
